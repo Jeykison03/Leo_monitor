2bbea57bf63828e01f24a1dd269b58dfe297d8ba
Jeykison03 <jeykison2013@gmail.com>
2026-01-29 21:13:50 +0100

    Add proxy configuration for API and WebSocket

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,5 +16,16 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/ws': {
+        target: 'ws://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 })
