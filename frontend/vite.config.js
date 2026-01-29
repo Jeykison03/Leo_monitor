09291df4fe0d4106c32748086bfc26ddd4ae4c10
Jeykison03 <jeykison2013@gmail.com>
2026-01-29 21:25:05 +0100

    Remove 'open' option from Vite server config
    Removed the 'open' option from the server configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -15,7 +14,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true
+   
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
