d724898a911563286ddb06831885d68a5edcd1d1
Jeykison03 <jeykison2013@gmail.com>
2026-01-29 21:14:22 +0100

    Change server binding from localhost to all interfaces

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -55,7 +55,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(app_state.clone())
             .configure(routes::config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
